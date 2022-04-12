66b1b14b0ec77255416ed03844b8bafabd1c5cb3
Md Rahman <Obaidqa86@gmail.com>
2022-04-12 00:23:04 -0400

    04/10/2022

---
+++ src/com/triangle/DataType.java
@@ -0,0 +1,11 @@
+package com.triangle;
+
+public class DataType {
+
+    public static void main(String[] args) {
+        int dateOfBirth = 1971;
+        int phoneNumber = 54854;
+        String address = "148 25  - 89th ave";
+        System.out.println(dateOfBirth + "\n " + phoneNumber + "\n" + address);
+    }
+}
